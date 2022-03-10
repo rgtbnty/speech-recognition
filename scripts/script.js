1d50f95d6ca7973fcf1efc33d61db8abc1a275d2
rgtbnty <codypurin26@gmail.com>
2022-03-11 06:23:23 +0900

    css and javascript written external file

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,58 @@
-var recog = new SpeechRecognitioin(); 
-const txt = document.getElementById("txt");
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+		const speech = new SpeechRecognition();
+		speech.interimResults = true;
+		speech.lang = 'ja-JP';
+		
+	/*	const btn = document.getElementById('btn');*/
+	/*	const content = document.getElementById('content'); */
+		const fi = document.getElementById('first');
+		const s = document.getElementById('second');
+		const t = document.getElementById('third');
+		const fo = document.getElementById('four');
+		
+		const test = document.getElementById('test');
+/*
+		btn.addEventListener('click', function() {
+		*/	speech.start(); /*
+		});
+		*/		
 
-recog.continuous = true;
-recog.lang = 'ja-JP';
-recog.intermiResults = true;
-recog.maxAlternatives = 1;
+		var m = 2;
 
-recog.onsoundstart = function() {
-	console.log('some sound is detected');
-}
+/*		var a_text = event.results[0][0].transcript
+		a_text = text.innerHTML;
+*/
+		let final = '';
+		speech.onresult = function(e) {
+		/*	speech.stop();*/
+			let itim = '';
+			for (var i = e.resultIndex; i < e.results.length; ++i) {
+				let transcript = e.results[i][0].transcript;
+				if (e.results[i].isFinal) {
+					final += transcript;
+				} else {
+					itim = transcript;
+					test.innerHTML = itim;
+				}
+			}
+			
+			/* test.innerHTML = e.results[0][0].transcript */
 
-btn.addEventListener("click", () => {
-	recog.start();
-	console.log('Speech!');
-});
+			if (e.results[0].isFinal) {
+				var autotext = e.results[0][0].transcript
+				console.log(e);
+				console.log(autotext);
+			/*	content.innerHTML += '<div>' + autotext + '</div>';*/
+				
+				if (m == 2) {
+					fo.innerHTML = t.innerHTML;
+					t.innerHTML = s.innerHTML;
+					s.innerHTML = fi.innerHTML;
+					fi.innerHTML = autotext;
+				}
+			}
+	}	
 
-recog.onresult = function(event) {
-	txt.innerHTML = event.results[0][0].transcript;
-	console.log('event.results[0][0])';
-}
+		speech.onend = () => {
+			speech.start();
+		};
